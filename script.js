2e998cad04f2e79990de8acc0ff871899efe325f
Tyler Thietje <tylerthietje@gmail.com>
2022-09-03 19:49:39 -0500

    finished the UI update for the game.

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-let playerScore = 0;
-let computerScore = 0;
-const buttons = document.querySelectorAll('button');
 let playerSelection;
-const computerSelection = getComputerChoice();
+let computerSelection;
+let roundWinner = '';
+let computerScore = 0;
+let playerScore = 0;
+const buttons = document.querySelectorAll('button');
+const playerScoreDisplay = document.querySelector('.player-score');
+const computerScoreDisplay = document.querySelector('.computer-score');
+const playerChoice = document.querySelector('.player-selection');
+const computerChoice = document.querySelector('.computer-selection');
+const playerPoints = document.querySelector('.player-points');
+const computerPoints = document.querySelector('.computer-points');
+const resultsMsg = document.querySelector('.result');
+const rules = document.querySelector('.rules');
 
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     playerSelection = button.id;
-    console.log(playerSelection);
+    computerSelection = getComputerChoice();
     playRound(playerSelection, computerSelection);
+    playerEmoji();
+    computerEmoji();
+    updateScore(roundWinner);
+    checkWinner();
   });
 });
 
@@ -18,78 +31,84 @@ function getComputerChoice() {
   return outcome[rdmNum];
 }
 
-function playRound(playerSelection, computerSelection) {
-  if (
-    playerSelection.toLowerCase() == 'rock' &&
-    computerSelection == 'scissors'
-  ) {
-    playerScore++;
-    return 'You WIN! Rock smashes Scissors!';
-  } else if (
-    playerSelection.toLowerCase() == 'rock' &&
-    computerSelection == 'paper'
-  ) {
-    computerScore++;
-    return 'You LOSE!! Paper covers Rock!';
-  } else if (
-    playerSelection.toLowerCase() == 'rock' &&
-    computerSelection == 'rock'
-  ) {
-    return "It's a tie. Please play again!";
-  } else if (
-    playerSelection.toLowerCase() == 'paper' &&
-    computerSelection == 'rock'
-  ) {
-    playerScore++;
-    return 'You WIN! Paper covers Rock!';
-  } else if (
-    playerSelection.toLowerCase() == 'paper' &&
-    computerSelection == 'paper'
-  ) {
-    return "It's a tie. Please play again!";
-  } else if (
-    playerSelection.toLowerCase() == 'paper' &&
-    computerSelection == 'scissors'
-  ) {
-    computerScore++;
-    return 'You LOSE!! Scissors cut up paper!';
-  } else if (
-    playerSelection.toLowerCase() == 'scissors' &&
-    computerSelection == 'rock'
-  ) {
-    computerScore++;
-    return 'You LOSE!! Rock smashes Scissors!';
-  } else if (
-    playerSelection.toLowerCase() == 'scissors' &&
-    computerSelection == 'paper'
-  ) {
-    playerScore++;
-    return 'You WIN!! Scissors cut up paper!';
-  } else if (
-    playerSelection.toLowerCase() == 'scissors' &&
-    computerSelection == 'scissors'
-  ) {
-    return "It's a tie. Please play again!";
+function playerEmoji() {
+  if (playerSelection === 'rock') {
+    playerChoice.innerText = `✊`;
+  }
+  if (playerSelection === 'paper') {
+    playerChoice.innerText = `✋`;
+  }
+  if (playerSelection === 'scissors') {
+    playerChoice.innerText = `✌`;
   }
 }
 
-function game() {
-  console.log(playRound(playerSelection, computerSelection));
-  console.log(
-    `Player chooses ${playerSelection}. Player score is ${playerScore}`
-  );
-  console.log(
-    `Computer chooses ${computerSelection}. Computer score is ${computerScore}`
-  );
-  console.log('-----------------------------------');
-  playRound(playerSelection, computerSelection);
+function computerEmoji() {
+  if (computerSelection === 'rock') {
+    computerChoice.innerText = `✊`;
+  }
+  if (computerSelection === 'paper') {
+    computerChoice.innerText = `✋`;
+  }
+  if (computerSelection === 'scissors') {
+    computerChoice.innerText = `✌`;
+  }
+}
 
+function playRound(playerSelection, computerSelection) {
+  resultsMsg.style.color = 'white';
+  if (playerSelection === computerSelection) {
+    return (roundWinner = 'tie');
+  } else if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper') ||
+    (playerSelection === 'paper' && computerSelection === 'rock')
+  ) {
+    playerScore++;
+    return (roundWinner = 'player');
+  } else if (
+    (computerSelection === 'rock' && playerSelection === 'scissors') ||
+    (computerSelection === 'scissors' && playerSelection === 'paper') ||
+    (computerSelection === 'paper' && playerSelection === 'rock')
+  ) {
+    computerScore++;
+    return (roundWinner = 'computer');
+  }
+}
 
-  if (playerScore > computerScore) {
-    return 'Congratulations! You beat the computer!';
-  } else if (computerScore > playerScore) {
-    return 'Uh Oh! The computer got you. WOMP WOMP!!';
+function updateScore(roundWinner) {
+  rules.style.display = 'none';
+  if (roundWinner === 'player') {
+    playerPoints.innerText = ` ${playerScore}`;
+    resultsMsg.innerText = `You Won! ${playerSelection.charAt(0).toUpperCase() + playerSelection.substring(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.substring(1)}!`;
+    
+  } else if (roundWinner === 'computer') {
+    computerPoints.innerText = ` ${computerScore}`;
+    resultsMsg.innerText = `You Lost! ${computerSelection.charAt(0).toUpperCase() + computerSelection.substring(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.substring(1)}!`;
   } else {
-    return "It's a tie, give it another go!";
-  };
-};
+    resultsMsg.innerText = `You tied! Throw again!`;
+  }
+
+}
+
+function checkWinner() {
+  if (playerScore === 5) {
+    resultsMsg.innerText = `You Have Won the Match!`;
+    resultsMsg.style.color = '#1a79d9';
+    rules.innerText = `Throw Again to Start a New Match!`;
+    rules.style.display = 'block';
+    playerScore = 0;
+    computerScore = 0;
+    playerPoints.innerText = ` ${playerScore}`;
+    computerPoints.innerText = ` ${computerScore}`;
+  } else if (computerScore === 5) {
+    resultsMsg.innerText = `You Have Lost the Match!`
+    resultsMsg.style.color = '#1a79d9';
+    rules.innerText = `Throw Again to Start a New Match!`;
+    rules.style.display = 'block';
+    computerScore = 0;
+    playerScore = 0;
+    playerPoints.innerText = ` ${playerScore}`;
+    computerPoints.innerText = ` ${computerScore}`;
+  }
+}
